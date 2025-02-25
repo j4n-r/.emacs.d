199652bb65d7f5b66e59a83ea4f7943446076847
j4n-r <jan.ruegge@gmail.com>
2025-02-25 18:02:19 +0100

    fix config

--- custom.el
+++ custom.el
@@ -4,8 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-vc-selected-packages
-   '((pgmacs :vc-backend Git :url "https://github.com/emarsden/pgmacs")
-     (pg :vc-backend Git :url "https://github.com/emarsden/pg-el"))))
+   '((pgmacs :url "https://github.com/emarsden/pgmacs/")
+     (pg :url "https://github.com/emarsden/pg-el/"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
