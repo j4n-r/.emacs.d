a786bf8f60b2cb0acdf5a608b4c9f8fb473f0b99
j4n-r <jan.rueggeberg@pm.me>
2025-11-13 09:34:31 +0100

    add missing files

---
+++ lisp/init-go.el
@@ -0,0 +1,12 @@
+;;; init-go.el --- Nix mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+;; (use-package go-mode
+;;   :ensure t
+;;   :mode "\\.go\\'"
+;;   :defer t)
+;; (setq go-ts-mode-hook go-mode-hook)
+(setq go-ts-mode-indent-offset 4)
+(provide 'init-go)
+;;; init-go.el ends here.
